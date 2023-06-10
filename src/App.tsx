7b057da3f5108b1ee48e1688e5446fc5b6db2bbd
Joao Gabriel Gangorra dos Santos <jggsantos.25@gmail.com>
2023-06-10 02:05:07 -0300

    feat: react router added

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,24 @@
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import SignUp from './pages/SignUp';
+import SignIn from './pages/SignIn';
+
 function App() {
 
 
   return (
     <>
-      <a href="">Olá, mundo!</a> 
+      <Router>
+            <Routes>
+              <Route path="/" element={<HomePage/>} />
+              <Route path="/sign-up" element={<SignUp />} />
+              <Route path="/sign-in" element={<SignIn />} />
+            </Routes>
+          </Router>
     </>
   )
 }
