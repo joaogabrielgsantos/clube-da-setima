637227a5ea0f71e463147cedf5f0754f93993e35
Joao Gabriel Gangorra dos Santos <jggsantos.25@gmail.com>
2023-07-21 05:55:19 -0300

    feat: docker added in backend with errors

--- backend/src/config/envs.ts
+++ backend/src/config/envs.ts
@@ -4,6 +4,8 @@ export function loadEnvs(){
     let path = ".env";
     if (process.env.NODE_ENV === "test"){
         path =".env.test"
+    } else if (process.env.NODE_ENV === "development") {
+        path = ".env.development";
     }
     dotenv.config({path})
 }
